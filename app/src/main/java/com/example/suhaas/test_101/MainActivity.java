b2111ce7f5655887ce20e2852e2701382d1889d7
suhaas-livcd <suhaas95@gmail.com>
2017-12-09 23:54:06 +0530

    Build Home screen (main activity)
    Signed-off-by: suhaas-livcd <suhaas95@gmail.com>

--- app/src/main/java/com/example/suhaas/test_101/MainActivity.java
+++ app/src/main/java/com/example/suhaas/test_101/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.suhaas.test_101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("app_info","startedd");
          setContentView(R.layout.activity_main);
     }
 }
