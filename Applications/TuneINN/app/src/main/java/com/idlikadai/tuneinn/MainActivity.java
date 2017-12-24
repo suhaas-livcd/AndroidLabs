a63865518bd2c75100fe7a73837357835c9bd6ff
suhaas-livcd <suhaas95@gmail.com>
2017-12-25 02:28:47 +0530

    Added Media Control Panel
    Signed-off-by: suhaas-livcd <suhaas95@gmail.com>

--- Applications/TuneINN/app/src/main/java/com/idlikadai/tuneinn/MainActivity.java
+++ Applications/TuneINN/app/src/main/java/com/idlikadai/tuneinn/MainActivity.java
@@ -1,7 +1,10 @@
 package com.idlikadai.tuneinn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView textView = (TextView) findViewById(R.id.TextView_title);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
