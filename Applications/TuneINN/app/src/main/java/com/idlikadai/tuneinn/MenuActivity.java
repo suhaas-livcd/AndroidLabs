660e543ce9bf98f794babd2e51393595cfac7e9f
suhaas-livcd <suhaas95@gmail.com>
2017-12-25 03:05:40 +0530

    Color Media Planel
    Signed-off-by: suhaas-livcd <suhaas95@gmail.com>

--- Applications/TuneINN/app/src/main/java/com/idlikadai/tuneinn/MenuActivity.java
+++ Applications/TuneINN/app/src/main/java/com/idlikadai/tuneinn/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.idlikadai.tuneinn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void GoToPreviousActvity(View view){
+        Intent intent = new Intent(MenuActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void ChnagePlaytoPause(View view) {
+        ImageView imageView = (ImageView) findViewById(R.id.ImageViewMenuPlay);
+        imageView.setImageResource(R.drawable.ic_pause_black_24px);
     }
 }
